30c0213b5c3d9d586c9c1fcca991fe80d2688ba9
HExris <hexris@outlook.com>
2020-10-18 00:53:50 +0800

    Init view components: Random

---
+++ src/views/Random/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="random">
+    
+  </div>
+</template>
+
+<script lang="ts">
+const Random = {
+  name: "Random",
+  components: {},
+};
+export default Random;
+</script>
+
+<style lang="less" scoped>
+@import url("./index");
+</style>
