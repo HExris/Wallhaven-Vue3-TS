f5829295aae24e6f17c4a0b3815c3a6d552a8ebd
HExris <hexris@outlook.com>
2020-10-18 00:53:18 +0800

    Init view components: Toplist

---
+++ src/views/Toplist/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="toplist">
+    
+  </div>
+</template>
+
+<script lang="ts">
+const Toplist = {
+  name: "Toplist",
+  components: {},
+};
+export default Toplist;
+</script>
+
+<style lang="less" scoped>
+@import url("./index");
+</style>
