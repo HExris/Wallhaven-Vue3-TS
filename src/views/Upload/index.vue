3a1d9c043e4ad0f43a2d1e4af4519ae8a7834397
HExris <hexris@outlook.com>
2020-10-18 00:52:30 +0800

    Init view components: Upload

---
+++ src/views/Upload/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="upload">
+    
+  </div>
+</template>
+
+<script lang="ts">
+const Upload = {
+  name: "Upload",
+  components: {},
+};
+export default Upload;
+</script>
+
+<style lang="less" scoped>
+@import url("./index");
+</style>
