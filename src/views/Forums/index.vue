e99c1ae3fddf40e47785752939dc5b3a65de15aa
HExris <hexris@outlook.com>
2020-10-18 00:54:16 +0800

    Init view components: Forums

---
+++ src/views/Forums/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="forums">
+    
+  </div>
+</template>
+
+<script lang="ts">
+const Forums = {
+  name: "Forums",
+  components: {}
+};
+export default Forums;
+</script>
+
+<style lang="less" scoped>
+@import url("./index");
+</style>
