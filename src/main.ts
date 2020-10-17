8cde267ed9672b85e0317f34c18725894032ab8f
HExris <hexris@outlook.com>
2020-10-18 00:55:42 +0800

    Import common style

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import store from './store'
 
 // Reset HTML element styles
 import "normalize.css";
+// Style Base
+import "@/assets/css/common.less";
 // Awesome Icons Pro
 import "@fortawesome/fontawesome-free/css/all.css";
 
+// Create Root Instance
 createApp(App).use(store).use(router).mount('#app')
