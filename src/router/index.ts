27533d60fc7cb3252bf99e52dd0833c0313a3d07
HExris <hexris@outlook.com>
2020-10-18 00:57:04 +0800

    Add routers

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,11 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
 import Layout from "@/Layout/index.vue";
 import Home from "@/views/Home/index.vue";
+import Latest from "@/views/Latest/index.vue";
+import Toplist from "@/views/Toplist/index.vue";
+import Random from "@/views/Random/index.vue";
+import Upload from "@/views/Upload/index.vue";
+import Forums from "@/views/Forums/index.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -13,6 +18,31 @@ const routes: Array<RouteRecordRaw> = [
         path: "/home",
         name: "Home",
         component: Home
+      },
+      {
+        path: "/latest",
+        name: "Latest",
+        component: Latest
+      },
+      {
+        path: "/Toplist",
+        name: "Toplist",
+        component: Toplist
+      },
+      {
+        path: "/random",
+        name: "Random",
+        component: Random
+      },
+      {
+        path: "/upload",
+        name: "Upload",
+        component: Upload
+      },
+      {
+        path: "/forums",
+        name: "Forums",
+        component: Forums
       }
     ]
   }
