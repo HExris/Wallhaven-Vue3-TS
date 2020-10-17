5195b7fcbf5d5bd51be8e0caeed32515b995a061
HExris <hexris@outlook.com>
2020-10-18 00:53:59 +0800

    Init view components: Latest

---
+++ src/views/Latest/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="lasted">
+    
+  </div>
+</template>
+
+<script lang="ts">
+const Latest = {
+  name: "Latest",
+  components: {}
+};
+export default Latest;
+</script>
+
+<style lang="less" scoped>
+@import url("./index");
+</style>
