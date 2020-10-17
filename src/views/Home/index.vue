b08beaa6a7091afab664ab39374cfaee95061f2b
HExris <hexris@outlook.com>
2020-10-18 00:57:17 +0800

    Add code

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,36 +1,16 @@
 <template>
   <div id="home">
-    <div class="homepage_menu">
-      <router-link
-        to="/"
-        class="brand"
-      >
-        <img src="~@/assets/images/logo.png" />
-        <h2 class="slogan"> The best wallpapers on the Net! </h2>
-      </router-link>
-      <ul>
-        <li
-          v-for="menu in homePageMenu"
-          :key="menu.title"
-        >{{menu.title}}</li>
-      </ul>
-      <h1>asdadsa</h1>
-    </div>
+    <NavMenu />
   </div>
 </template>
 
 <script lang="ts">
-import { useStore } from "vuex";
+import NavMenu from "@/components/Dumb/NavMenu/index.vue";
+
 const Home = {
   name: "Home",
-  components: {},
-  setup() {
-    const store = useStore()
-    // const menu = store.getters.getMenu()
-    console.log(store.getters.getMenu)
-    return store;
-  },
-}
+  components: { NavMenu },
+};
 export default Home;
 </script>
 
