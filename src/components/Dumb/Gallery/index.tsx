a3c954524069dbd5dcd85375aabe6e68b87ea64a
HExris <hexris@outlook.com>
2020-10-20 03:26:04 +0800

    Add code

--- src/components/Dumb/Gallery/index.tsx
+++ src/components/Dumb/Gallery/index.tsx
@@ -3,37 +3,46 @@ import getAllImgList from "@/api/getList.ts";
 import { reactive, defineComponent, onMounted, computed } from "vue";
 interface GalleryState {
     imgList: any;
+    topfourImgs: any;
+    restImgs: any;
 }
 export default defineComponent({
     name: "Gallery",
     setup() {
         const galleryState: GalleryState = reactive({
-            imgList: []
+            imgList: [],
+            topfourImgs: [],
+            restImgs: []
         })
-        const topfourImgs = computed(() => {
-            galleryState.imgList.value.splice(0, 4);
-        })
-        const restImgs = computed(() => { galleryState.imgList.value.splice(4, -1); })
+
         onMounted(() => {
             getAllImgList().then(res => {
-                galleryState.imgList = res
+                if (res instanceof Array) {
+                    galleryState.imgList = [...res]
+                    galleryState.topfourImgs = [...res].slice(0, 4);
+                    galleryState.restImgs = [...res].slice(4);
+                }
             })
         })
+
         const renderImgList = (list: any) => {
             return (
-                list instanceof Array && list.map((v, index) => (
-                    <div class="image_wrapper" key={index}>
-                        <div class="image_placeholder"></div>
-                        <img src={v} />
-                    </div>)
+                list instanceof Array && list.map((v, index) => {
+                    return v && (
+                        <div class="image_wrapper" key={index} >
+                            <div class="image_placeholder"></div>
+                            <img src={v} />
+                        </div >
+                    )
+                }
                 )
             )
         }
         const renderGallery = () => {
             return (
                 <div class="gallery">
-                    { renderImgList(galleryState.imgList)}
-                    { renderImgList(restImgs)}
+                    { renderImgList(galleryState.topfourImgs)}
+                    { renderImgList(galleryState.restImgs)}
                 </div>
             )
         }
